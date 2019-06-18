e9139337f520ae0ca099b2a1354ff7fabafa9772
duneoz <nicholas.charles.meyers@gmail.com>
2019-06-18 16:40:10 -0700

    followed initial instructions, created files and initial js code

---
+++ liri.js
@@ -0,0 +1,25 @@
+// read and set any environment variables with the dotenv package
+require("dotenv").config();
+
+//import keys.js, store in variable
+var keys = require("./keys.js");
+
+//node commands for LIRI
+function UserInputs (userOption, inputParameter){
+    switch (userOption) {
+    case 'concert-this':
+        showConcertInfo(inputParameter);
+        break;
+    case 'spotify-this-song':
+        showSongInfo(inputParameter);
+        break;
+    case 'movie-this':
+        showMovieInfo(inputParameter);
+        break;
+    case 'do-what-it-says':
+        showSomeInfo();
+        break;
+    default: 
+        console.log("Invalid Option. Please type any of the following options: \nconcert-this \nspotify-this-song \nmovie-this \ndo-what-it-says")
+    }
+}
