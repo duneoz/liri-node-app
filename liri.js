29d58eae38c86fd10143d59ebfdb0ed46335df0a
duneoz <nicholas.charles.meyers@gmail.com>
2019-06-18 16:43:33 -0700

    added initial variables

--- liri.js
+++ liri.js
@@ -4,6 +4,14 @@ require("dotenv").config();
 //import keys.js, store in variable
 var keys = require("./keys.js");
 
+var spotify = new Spotify(keys.spotify);
+
+//user inputs
+var userOption = process.argv[2];
+var inputParameter = process.argv[3];
+
+UserInputs(userOption, inputParameter);
+
 //node commands for LIRI
 function UserInputs (userOption, inputParameter){
     switch (userOption) {
