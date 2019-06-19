f5162e3732cd6cdcd716244e9f7fdc4e980ee07a
duneoz <nicholas.charles.meyers@gmail.com>
2019-06-18 18:07:18 -0700

    movie-this and concert-this-axios node commands working

--- liri.js
+++ liri.js
@@ -4,10 +4,13 @@ require("dotenv").config();
 //import keys.js, store in variable
 var keys = require("./keys.js");
 
+var axios = require('axios');
 var request = require("request");
 var fs = require("fs");
 var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
+var moment = require('moment');
+moment().format();
 
 //user inputs
 var userOption = process.argv[2];
@@ -16,52 +19,81 @@ var inputParameter = process.argv[3];
 UserInputs(userOption, inputParameter);
 
 //node commands for LIRI
-function UserInputs (userOption, inputParameter){
+function UserInputs(userOption, inputParameter) {
     switch (userOption) {
-    case 'concert-this':
-        showConcertInfo(inputParameter);
-        break;
-    case 'spotify-this-song':
-        showSongInfo(inputParameter);
-        break;
-    case 'movie-this':
-        showMovieInfo(inputParameter);
-        break;
-    case 'do-what-it-says':
-        showSomeInfo();
-        break;
-    default: 
-        console.log("Invalid Option. Please type any of the following options: \nconcert-this \nspotify-this-song \nmovie-this \ndo-what-it-says")
+        case 'concert-this':
+            showConcertInfo(inputParameter);
+            break;
+        case 'concert-this-axios':
+            concertThis(inputParameter);
+            break;
+        case 'spotify-this-song':
+            showSongInfo(inputParameter);
+            break;
+        case 'movie-this':
+            showMovieInfo(inputParameter);
+            break;
+        case 'do-what-it-says':
+            showSomeInfo();
+            break;
+        default:
+            console.log("Invalid Option. Please type any of the following options: \nconcert-this \nspotify-this-song \nmovie-this \ndo-what-it-says")
     }
 }
 
 
 //function to support the concert-this node command
-function showConcertInfo(inputParameter){
+function showConcertInfo(inputParameter) {
     var queryUrl = "https://rest.bandsintown.com/artists/" + inputParameter + "/events?app_id=codingbootcamp";
-    request(queryUrl, function(error, response, body) {
-    // If the request is successful
-    if (!error && response.statusCode === 200) {
-        var concerts = JSON.parse(body);
-        for (var i = 0; i < concerts.length; i++) {  
-            console.log("**********EVENT INFO*********");  
-            fs.appendFileSync("log.txt", "**********EVENT INFO*********\n");//Append in log.txt file
-            console.log(i);
-            fs.appendFileSync("log.txt", i+"\n");
-            console.log("Name of the Venue: " + concerts[i].venue.name);
-            fs.appendFileSync("log.txt", "Name of the Venue: " + concerts[i].venue.name+"\n");
-            console.log("Venue Location: " +  concerts[i].venue.city);
-            fs.appendFileSync("log.txt", "Venue Location: " +  concerts[i].venue.city+"\n");
-            console.log("Date of the Event: " +  concerts[i].datetime);
-            fs.appendFileSync("log.txt", "Date of the Event: " +  concerts[i].datetime+"\n");
-            console.log("*****************************");
-            fs.appendFileSync("log.txt", "*****************************"+"\n");
-        }
-    } else{
-      console.log('Error occurred.');
-    }
-});}
+    request(queryUrl, function (error, response, body) {
 
+        // axios.get(queryUrl)
+        // .then(function (response) {
+        // If the request is successful
+        if (!error && response.statusCode === 200) {
+            var concerts = JSON.parse(body);
+            for (var i = 0; i < concerts.length; i++) {
+                console.log("**********EVENT INFO*********");
+                fs.appendFileSync("log.txt", "**********EVENT INFO*********\n");//Append in log.txt file
+                console.log(i);
+                fs.appendFileSync("log.txt", i + "\n");
+                console.log("Name of the Venue: " + concerts[i].venue.name);
+                fs.appendFileSync("log.txt", "Name of the Venue: " + concerts[i].venue.name + "\n");
+                console.log("Venue Location: " + concerts[i].venue.city);
+                fs.appendFileSync("log.txt", "Venue Location: " + concerts[i].venue.city + "\n");
+                console.log("Date of the Event: " + concerts[i].datetime);
+                fs.appendFileSync("log.txt", "Date of the Event: " + concerts[i].datetime + "\n");
+                console.log("*****************************");
+                fs.appendFileSync("log.txt", "*****************************" + "\n");
+            }
+        } else {
+            console.log('Error occurred.');
+        }
+    });
+}
+
+
+//concert-this-axios node command
+function concertThis(inputParameter) {
+    axios.get("https://rest.bandsintown.com/artists/" + inputParameter + "/events?app_id=codingbootcamp")
+        .then(function (response) {
+            for (var i = 0; i < response.data.length; i++) {
+
+                var datetime = response.data[i].datetime; //Saves datetime response into a variable
+                var dateArr = datetime.split('T'); //Attempting to split the date and time in the response
+
+                var concertResults =
+                    "--------------------------------------------------------------------" +
+                    "\nVenue Name: " + response.data[i].venue.name +
+                    "\nVenue Location: " + response.data[i].venue.city +
+                    "\nDate of the Event: " + moment(dateArr[0], "MM-DD-YYYY"); //dateArr[0] should be the date separated from the time
+                console.log(concertResults);
+            }
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+}
 
 //function to support the spotify-this-song node command
 function showSongInfo(inputParameter) {
@@ -84,69 +116,70 @@ function showSongInfo(inputParameter) {
                 console.log("**********SONG INFO*********");
                 fs.appendFileSync("log.txt", "**********SONG INFO*********\n");
                 console.log(i);
-                fs.appendFileSync("log.txt", i +"\n");
+                fs.appendFileSync("log.txt", i + "\n");
                 console.log("Song name: " + songs[i].name);
-                fs.appendFileSync("log.txt", "song name: " + songs[i].name +"\n");
+                fs.appendFileSync("log.txt", "song name: " + songs[i].name + "\n");
                 console.log("Preview song: " + songs[i].preview_url);
-                fs.appendFileSync("log.txt", "preview song: " + songs[i].preview_url +"\n");
+                fs.appendFileSync("log.txt", "preview song: " + songs[i].preview_url + "\n");
                 console.log("Album: " + songs[i].album.name);
                 fs.appendFileSync("log.txt", "album: " + songs[i].album.name + "\n");
                 console.log("Artist(s): " + songs[i].artists[0].name);
                 fs.appendFileSync("log.txt", "artist(s): " + songs[i].artists[0].name + "\n");
-                console.log("*****************************");  
+                console.log("*****************************");
                 fs.appendFileSync("log.txt", "*****************************\n");
-             }
+            }
         }
     );
 };
 
 //function to support the movie-this node command
-function showMovieInfo(inputParameter){
+function showMovieInfo(inputParameter) {
     if (inputParameter === undefined) {
         inputParameter = "The Sandlot"
         console.log("-----------------------");
         fs.appendFileSync("log.txt", "-----------------------\n");
         console.log("You're KILLNG me smalls: https://www.imdb.com/title/tt0108037/?ref_=fn_al_tt_1");
-        fs.appendFileSync("log.txt", "You're KILLNG me smalls: https://www.imdb.com/title/tt0108037/?ref_=fn_al_tt_1" +"\n");
+        fs.appendFileSync("log.txt", "You're KILLNG me smalls: https://www.imdb.com/title/tt0108037/?ref_=fn_al_tt_1" + "\n");
         console.log("Buy it.");
         fs.appendFileSync("log.txt", "Buy it.\n");
     }
     var queryUrl = "http://www.omdbapi.com/?t=" + inputParameter + "&y=&plot=short&apikey=b3c0b435";
-    request(queryUrl, function(error, response, body) {
-    // If the request is successful
-    if (!error && response.statusCode === 200) {
-        var movies = JSON.parse(body);
-        console.log("**********MOVIE INFO*********");  
-        fs.appendFileSync("log.txt", "**********MOVIE INFO*********\n");
-        console.log("Title: " + movies.Title);
-        fs.appendFileSync("log.txt", "Title: " + movies.Title + "\n");
-        console.log("Release Year: " + movies.Year);
-        fs.appendFileSync("log.txt", "Release Year: " + movies.Year + "\n");
-        console.log("IMDB Rating: " + movies.imdbRating);
-        fs.appendFileSync("log.txt", "IMDB Rating: " + movies.imdbRating + "\n");
-        console.log("Country of Production: " + movies.Country);
-        fs.appendFileSync("log.txt", "Country of Production: " + movies.Country + "\n");
-        console.log("Language: " + movies.Language);
-        fs.appendFileSync("log.txt", "Language: " + movies.Language + "\n");
-        console.log("Plot: " + movies.Plot);
-        fs.appendFileSync("log.txt", "Plot: " + movies.Plot + "\n");
-        console.log("Actors: " + movies.Actors);
-        fs.appendFileSync("log.txt", "Actors: " + movies.Actors + "\n");
-        console.log("*****************************");  
-        fs.appendFileSync("log.txt", "*****************************\n");
-    } else{
-      console.log('Error occurred.');
-    }
+    request(queryUrl, function (error, response, body) {
+        // If the request is successful
+        if (!error && response.statusCode === 200) {
+            var movies = JSON.parse(body);
+            console.log("**********MOVIE INFO*********");
+            fs.appendFileSync("log.txt", "**********MOVIE INFO*********\n");
+            console.log("Title: " + movies.Title);
+            fs.appendFileSync("log.txt", "Title: " + movies.Title + "\n");
+            console.log("Release Year: " + movies.Year);
+            fs.appendFileSync("log.txt", "Release Year: " + movies.Year + "\n");
+            console.log("IMDB Rating: " + movies.imdbRating);
+            fs.appendFileSync("log.txt", "IMDB Rating: " + movies.imdbRating + "\n");
+            console.log("Country of Production: " + movies.Country);
+            fs.appendFileSync("log.txt", "Country of Production: " + movies.Country + "\n");
+            console.log("Language: " + movies.Language);
+            fs.appendFileSync("log.txt", "Language: " + movies.Language + "\n");
+            console.log("Plot: " + movies.Plot);
+            fs.appendFileSync("log.txt", "Plot: " + movies.Plot + "\n");
+            console.log("Actors: " + movies.Actors);
+            fs.appendFileSync("log.txt", "Actors: " + movies.Actors + "\n");
+            console.log("*****************************");
+            fs.appendFileSync("log.txt", "*****************************\n");
+        } else {
+            console.log('Error occurred.');
+        }
 
-});}
+    });
+}
 
 
 //function for the do-what-it-says node command
-function showSomeInfo(){
-	fs.readFile('random.txt', 'utf8', function(err, data){
-		if (err){ 
-			return console.log(err);
-		}
+function showSomeInfo() {
+    fs.readFile('random.txt', 'utf8', function (err, data) {
+        if (err) {
+            return console.log(err);
+        }
         var dataArr = data.split(',');
         UserInputs(dataArr[0], dataArr[1]);
     });
