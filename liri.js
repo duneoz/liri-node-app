437fa910aff549c53448b5356e0d96f4a1e7ba1a
duneoz <nicholas.charles.meyers@gmail.com>
2019-06-18 17:13:29 -0700

    spotify-this-song node command working

--- liri.js
+++ liri.js
@@ -4,6 +4,8 @@ require("dotenv").config();
 //import keys.js, store in variable
 var keys = require("./keys.js");
 
+var fs = require("fs");
+var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 
 //user inputs
@@ -58,3 +60,95 @@ function showConcertInfo(inputParameter){
       console.log('Error occurred.');
     }
 });}
+
+
+//function to support the spotify-this-song node command
+function showSongInfo(inputParameter) {
+    if (inputParameter === undefined) {
+        inputParameter = "Smooth Criminal"; //default Song
+    }
+    spotify.search(
+        {
+            type: "track",
+            query: inputParameter
+        },
+        function (err, data) {
+            if (err) {
+                console.log("Error occurred: " + err);
+                return;
+            }
+            var songs = data.tracks.items;
+
+            for (var i = 0; i < songs.length; i++) {
+                console.log("**********SONG INFO*********");
+                fs.appendFileSync("log.txt", "**********SONG INFO*********\n");
+                console.log(i);
+                fs.appendFileSync("log.txt", i +"\n");
+                console.log("Song name: " + songs[i].name);
+                fs.appendFileSync("log.txt", "song name: " + songs[i].name +"\n");
+                console.log("Preview song: " + songs[i].preview_url);
+                fs.appendFileSync("log.txt", "preview song: " + songs[i].preview_url +"\n");
+                console.log("Album: " + songs[i].album.name);
+                fs.appendFileSync("log.txt", "album: " + songs[i].album.name + "\n");
+                console.log("Artist(s): " + songs[i].artists[0].name);
+                fs.appendFileSync("log.txt", "artist(s): " + songs[i].artists[0].name + "\n");
+                console.log("*****************************");  
+                fs.appendFileSync("log.txt", "*****************************\n");
+             }
+        }
+    );
+};
+
+//function to support the movie-this node command
+function showMovieInfo(inputParameter){
+    if (inputParameter === undefined) {
+        inputParameter = "The Sandlot"
+        console.log("-----------------------");
+        fs.appendFileSync("log.txt", "-----------------------\n");
+        console.log("You're KILLNG me smalls: https://www.imdb.com/title/tt0108037/?ref_=fn_al_tt_1");
+        fs.appendFileSync("log.txt", "You're KILLNG me smalls: https://www.imdb.com/title/tt0108037/?ref_=fn_al_tt_1" +"\n");
+        console.log("Buy it.");
+        fs.appendFileSync("log.txt", "Buy it.\n");
+    }
+    var queryUrl = "http://www.omdbapi.com/?t=" + inputParameter + "&y=&plot=short&apikey=b3c0b435";
+    request(queryUrl, function(error, response, body) {
+    // If the request is successful
+    if (!error && response.statusCode === 200) {
+        var movies = JSON.parse(body);
+        console.log("**********MOVIE INFO*********");  
+        fs.appendFileSync("log.txt", "**********MOVIE INFO*********\n");
+        console.log("Title: " + movies.Title);
+        fs.appendFileSync("log.txt", "Title: " + movies.Title + "\n");
+        console.log("Release Year: " + movies.Year);
+        fs.appendFileSync("log.txt", "Release Year: " + movies.Year + "\n");
+        console.log("IMDB Rating: " + movies.imdbRating);
+        fs.appendFileSync("log.txt", "IMDB Rating: " + movies.imdbRating + "\n");
+        console.log("Rotten Tomatoes Rating: " + getRottenTomatoesRatingValue(movies));
+        fs.appendFileSync("log.txt", "Rotten Tomatoes Rating: " + getRottenTomatoesRatingValue(movies) + "\n");
+        console.log("Country of Production: " + movies.Country);
+        fs.appendFileSync("log.txt", "Country of Production: " + movies.Country + "\n");
+        console.log("Language: " + movies.Language);
+        fs.appendFileSync("log.txt", "Language: " + movies.Language + "\n");
+        console.log("Plot: " + movies.Plot);
+        fs.appendFileSync("log.txt", "Plot: " + movies.Plot + "\n");
+        console.log("Actors: " + movies.Actors);
+        fs.appendFileSync("log.txt", "Actors: " + movies.Actors + "\n");
+        console.log("*****************************");  
+        fs.appendFileSync("log.txt", "*****************************\n");
+    } else{
+      console.log('Error occurred.');
+    }
+
+});}
+
+
+//function for the do-what-it-says node command
+function showSomeInfo(){
+	fs.readFile('random.txt', 'utf8', function(err, data){
+		if (err){ 
+			return console.log(err);
+		}
+        var dataArr = data.split(',');
+        UserInputs(dataArr[0], dataArr[1]);
+    });
+}
